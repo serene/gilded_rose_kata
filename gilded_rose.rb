e24a0874157a4b9980fdd8fa829e14b89a896c54
Serene Yew <serene@pixeltree.ca>
2021-06-04 10:36:08 -0600

    Add edge cases for backstage passes

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,57 +4,66 @@ class GildedRose
     @items = items
   end
 
+  AGED_BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
+        decrease_quality(item)
       else
-        if item.quality < 50
+        if item.quality < Item::MAX_QUALITY
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+          if item.name == BACKSTAGE_PASS
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
+            
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
           end
         end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
       end
 
+      item.sell_in = item.sell_in - 1 unless item.name == SULFURAS
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+        if item.name != AGED_BRIE
+          if item.name != BACKSTAGE_PASS
+            decrease_quality(item)
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_quality(item)
         end
       end
+    end
+  end
+
+  private
+
+  def decrease_quality(item)
+    if item.quality > 0
+      if item.name != SULFURAS
+        item.quality = item.quality - 1
+      end
+    end
+  end
+
+  def increase_quality(item)
+    if item.quality < Item::MAX_QUALITY
+      item.quality = item.quality + 1
     end
   end
 end
 
 class Item
+  MAX_QUALITY = 50
+
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -6,49 +6,78 @@ describe GildedRose do
   describe "#update_quality" do
     subject { GildedRose.new([item]).update_quality }
 
-    let(:item) { Item.new(name, 10, 10) }
-
-    context 'when is "Aged Brie"' do
-      let(:name) { "Aged Brie" }
-
-      it 'decreases sell in and increases quality' do
-        subject
-
-        expect(item.sell_in).to eq 9
-        expect(item.quality).to eq 11
-      end
-    end
-
     context 'when is "Backstage passes to a TAFKAL80ETC concert"' do
       let(:name) { "Backstage passes to a TAFKAL80ETC concert" }
 
-      it 'decreases sell in and increases quality' do
-        subject
+      context 'when sell in is 11' do
+        let(:item) { Item.new(name, 11, 10) }
 
-        expect(item.sell_in).to eq 9
-        expect(item.quality).to eq 12
+        it 'decreases sell in and increases quality' do
+          subject
+
+          expect(item.sell_in).to eq 10
+          expect(item.quality).to eq 11
+        end
+      end
+
+      context 'when sell in is 10' do
+        let(:item) { Item.new(name, 10, 10) }
+
+        it 'decreases sell in and increases quality by 2' do
+          subject
+
+          expect(item.sell_in).to eq 9
+          expect(item.quality).to eq 12
+        end
+      end
+
+      context 'when sell in is 5' do
+        let(:item) { Item.new(name, 5, 10) }
+
+        it 'decreases sell in and increases quality by 3' do
+          subject
+
+          expect(item.sell_in).to eq 4
+          expect(item.quality).to eq 13
+        end
       end
     end
 
-    context 'when is "Sulfuras, Hand of Ragnaros"' do
-      let(:name) { "Sulfuras, Hand of Ragnaros" }
+    describe 'edge cases for special items' do
+      let(:item) { Item.new(name, 10, 10) }
 
-      it 'does not change sell in or quality' do
-        subject
+      context 'when is "Aged Brie"' do
+        let(:name) { "Aged Brie" }
 
-        expect(item.sell_in).to eq 10
-        expect(item.quality).to eq 10
+        it 'decreases sell in and increases quality' do
+          subject
+
+          expect(item.sell_in).to eq 9
+          expect(item.quality).to eq 11
+        end
       end
-    end
 
-    context 'when is some other item' do
-      let(:name) { "Orchids" }
 
-      it 'decreases sell in and qualit' do
-        subject
+      context 'when is "Sulfuras, Hand of Ragnaros"' do
+        let(:name) { "Sulfuras, Hand of Ragnaros" }
 
-        expect(item.sell_in).to eq 9
-        expect(item.quality).to eq 9
+        it 'does not change sell in or quality' do
+          subject
+
+          expect(item.sell_in).to eq 10
+          expect(item.quality).to eq 10
+        end
+      end
+
+      context 'when is some other item' do
+        let(:name) { "Orchids" }
+
+        it 'decreases sell in and quality' do
+          subject
+
+          expect(item.sell_in).to eq 9
+          expect(item.quality).to eq 9
+        end
       end
     end
   end
